afedc4ccd623c9794dbe722a4819a9350a3cd4cf
Graham Ornstein <gornstein123@gmail.com>
2024-02-28 14:50:54 -0500

    finished parts 4-7

--- project2.py
+++ project2.py
@@ -4,6 +4,7 @@ import librosa.display
 import librosa
 import soundfile as sf
 import sounddevice as sd
+from scipy.signal import butter,filtfilt
 
 duration = 5
 fs = sr = 48000
@@ -17,6 +18,11 @@ def create_spectrogram(audio,maxfreq,plotname):
     plt.title('Spectrogram - '+plotname)
     plt.savefig(plotname+"-SG.png")
     plt.show()
+
+def gen_tone(freq,dur):
+    t = np.linspace(0,dur,sr * dur, endpoint=False)
+    tone = np.sin(2 * np.pi * freq * t)
+    return tone
 
 # Generate time axis (for all problems)
 x = np.linspace(0, duration, sr * duration, endpoint=False)
@@ -47,31 +53,43 @@ create_spectrogram(chirp,9000,"0-8000Hz Chirp Signal")
 
 ## Some Fun with Sine Tone
 # Someone commented what the likely notes are in the YT video so these freqs
-# are for B4f,C4,G4#,G3#,Ef4
-notes = [466.2,261.6,415.3,207.7,311.1]
-secondspernote = np.array([0.4,0.6,0.85,0.6,1.57])
-samples = secondspernote * (sr*duration/5)
+# are for B4f,C5,G4#,G3#,Ef4
+notes = [466.2,523,415.3,207.7,311.1]
 
-freq3 = x
+# Generate the 5 tones
+note1 = gen_tone(notes[0],1)
+note2 = gen_tone(notes[1],1)
+note3 = gen_tone(notes[2],1)
+note4 = gen_tone(notes[3],1)
+note5 = gen_tone(notes[4],2)
 
-i_l = 0
-i_u = samples[0]
-current_note = 0
-# Multiply samples by their new frequencies
-for i in range(len(freq3)):
-    if i_l <= i < i_u:
-        freq3[i] = x[i]*notes[current_note]
-    else:
-        # Next note
-        current_note = current_note + 1
-        # For no note:
-        if current_note > len(secondspernote)-1:
-            freq3[i] = x[i]*0
-            continue
-        else:
-            i_l = i_u
-            i_u = i_u + samples[current_note]
-                
-sinetonefun = np.sin((2*np.pi)*freq3)
-sf.write("teamOrnstein-cetk.wav",sinetonefun,sr)
-create_spectrogram(sinetonefun,2000,"CETK Chord Progression")
+# Put them together
+cetk = np.concatenate([note1,note2,note3,note4,note5])
+
+sf.write("teamOrnstein-cetk.wav",cetk,sr)
+create_spectrogram(cetk,2000,"CETK Chord Progression")
+
+## Combining Sound Files
+quickfox, sr = sf.read("quickfox.wav")
+quickfox_5000 = quickfox + sinetone[0:192000,] # My quickfox is only 4s
+
+sf.write("teamOrnstein-speechchirp.wav",quickfox_5000,sr)
+create_spectrogram(quickfox_5000,8000,"Quickfox + 5000Hz Sine Tone")
+
+## Speech and audio filtering
+# Create a butterworth lowpass filter
+# order=5, cutoff=4000Hz
+b,a = butter(5,(4000/(sr*0.5)),btype='low',analog=False)
+
+# Filter the quickfox+sinewave
+filtered_quickfox_5000 = filtfilt(b,a,quickfox_5000)
+
+sf.write("teamOrnstein-filteredspeechsine.wav",filtered_quickfox_5000,sr)
+create_spectrogram(filtered_quickfox_5000,8000,"Quickfox + 5000Hz Sine + 4000Hz Lowpass")
+
+## Stereo Fun
+# Combine the two signals to stereo
+stereo_fun = np.asarray([quickfox,quickfox_5000]).T
+# Spectrograms are already generated for each for report
+
+sf.write("teamOrnstein-stereospeechsine.wav",stereo_fun,sr)
